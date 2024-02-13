f57f5e2f82f8a789fdb16db9d2ee8d1d660db45a
Umutcan-YEL <umutcanyel132@gmail.com>
2024-02-13 11:28:25 +0300

    minor change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import { useDispatch } from "react-redux";
 import { gettasks } from "./services/redux/slices/TaskSlice";
 import { AppDispatch } from "./services/redux/Store";
 import { getboards } from "./services/redux/slices/BoardSlice";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 const Home = lazy(() => import("./pages/Home"));
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
-  dispatch(getboards());
-  dispatch(gettasks());
+
+  useEffect(() => {
+    dispatch(getboards());
+    dispatch(gettasks());
+    
+  });
 
   return (
     <Suspense
